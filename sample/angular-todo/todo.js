e21e5eda9c14c1a7b320f1c23cda4dbd9cdc588d
schettino72 <schettino72@gmail.com>
2012-10-28 17:11:57 +0800

    todo.js change js style

--- sample/angular-todo/todo.js
+++ sample/angular-todo/todo.js
@@ -6,23 +6,22 @@ function TodoItem(text, done){
 
     this.$text = null; // containing the todo text
     this.$input = null; // containing the checkbox
-
-    this.render = function(){
-        this.$input = input({type: "checkbox"}).prop('checked', this.done);
-        this.on(this.$input, 'click', function(){
-            this._set_done();
-            this.trigger('changed', this.done);
-        });
-        this.$text = span(this.text);
-        this._set_done();
-        return [this.$input, this.$text];
-    };
-    this._set_done = function(){
-        this.done = this.$input.prop('checked');
-        this.$text.addClass('done-' + this.done).removeClass('done-' + !this.done);
-    };
 }
 $.extend(TodoItem.prototype, hoe.obj_proto);
+
+TodoItem.prototype.render = function(){
+    this.$input = input({type: "checkbox"}).prop('checked', this.done);
+    this.on(this.$input, 'click', this.set_done);
+    this.$text = span(this.text).addClass('done-' + this.done);
+    return [this.$input, this.$text];
+};
+
+TodoItem.prototype.set_done = function(){
+    this.done = this.$input.prop('checked');
+    this.$text.addClass('done-' + this.done).removeClass('done-' + !this.done);
+    this.trigger('set_done', this.done);
+};
+
 
 
 function Todo() {
@@ -31,52 +30,54 @@ function Todo() {
 
     this.$remaining_title = span();
     this.$todo_list = ul({'class': "unstyled"});
-
-    this.addTodo = function(text, done) {
-        var item = new TodoItem(text || '', done || false);
-        this.on(item, 'changed', this.update);
-        this.remaining += item.done ? 0 : 1;
-        this.todos.push(item);
-        item.$ele = li(item.render());
-        this.$todo_list.append(item.$ele);
-    };
-
-    this.update = function(done) {
-        this.remaining += done ? -1 : 1;
-        this._render_remaining();
-    };
-
-    this.archive = function() {
-        var oldTodos = this.todos;
-        this.todos = [];
-        this.forEach(oldTodos, function(todo){
-            if (!todo.done)
-                this.todos.push(todo);
-            else
-                todo.$ele.remove();
-        });
-        this._render_remaining();
-    };
-
-    this._render_remaining = function(){
-        return this.$remaining_title.text(
-            this.remaining + ' of ' + this.todos.length + ' remaining');
-    };
-
-    this.render = function() {
-        var $archive_link = a({href: "#"}, 'archive');
-        this.on($archive_link, 'click', this.archive);
-        var $todo_input = input({type:"text", size:"30", placeholder:"add new todo here"});
-        var $addBtn = input({'class': "btn-primary", type:"submit", value:"add"});
-        this.on($addBtn, 'click', function(event){
-            this.addTodo($todo_input.val(), false);
-            this._render_remaining();
-            $todo_input.val('');
-            event.preventDefault();
-        });
-
-        return [this._render_remaining(), ' [', $archive_link, ']',
-                this.$todo_list, form($todo_input, $addBtn)];
-    };
 }
 $.extend(Todo.prototype, hoe.obj_proto);
+
+Todo.prototype.addTodo = function(text, done) {
+    var item = new TodoItem(text || '', done || false);
+    this.on(item, 'set_done', this.update);
+    this.remaining += item.done ? 0 : 1;
+    this.todos.push(item);
+    item.$ele = li(item.render());
+    this.$todo_list.append(item.$ele);
+};
+
+Todo.prototype.update = function(done) {
+    this.remaining += done ? -1 : 1;
+    this._render_remaining();
+};
+
+Todo.prototype.archive = function() {
+    var oldTodos = this.todos;
+    this.todos = [];
+    this.forEach(oldTodos, function(todo){
+        if (!todo.done)
+            this.todos.push(todo);
+        else
+            todo.$ele.remove();
+    });
+    this._render_remaining();
+};
+
+Todo.prototype._render_remaining = function(){
+    return this.$remaining_title.text(
+        this.remaining + ' of ' + this.todos.length + ' remaining');
+};
+
+Todo.prototype.render = function() {
+    var $archive_link = a({href: "#"}, 'archive');
+    this.on($archive_link, 'click', this.archive);
+    var $todo_input = input({type:"text", size:"30", placeholder:"add new todo here"});
+    var $addBtn = input({'class': "btn-primary", type:"submit", value:"add"});
+    this.on($addBtn, 'click', function(event){
+        this.addTodo($todo_input.val(), false);
+        this._render_remaining();
+        $todo_input.val('');
+        event.preventDefault();
+    });
+
+    return [this._render_remaining(), ' [', $archive_link, ']',
+            this.$todo_list,
+            form($todo_input, $addBtn)];
+};
+
